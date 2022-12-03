5a8451764960c8851fe58194b34c711d42ad50c7
ahmed-tabib <ahmed.tabib0@gmail.com>
2022-12-03 14:27:53 +0100

    added fetching and caching subdomains from chaos

--- snake_charmer.py
+++ snake_charmer.py
@@ -24,6 +24,52 @@ def get_chaos_list(force_fetch=False):
         cache_file.write(chaos_list_response.text)
     return json.loads(chaos_list_response.text)
 
-#generate the url of the zip file containing subdomains for a specific program name
-def get_subdomain_file_url(program_name):
-    return "https://chaos-data.projectdiscovery.io/" + program_name.lower().replace(' ', '_') + ".zip"
+#get the subdomain list from chaos for a specific program
+def get_chaos_subdomains(program_name):
+    subdomain_file_name = program_name.lower().replace(' ', '_') + ".zip"
+    cached_and_valid = False
+
+    #use cached file if 24 hours hadn't passed since fetching
+    if os.path.exists("subdomain_dir/"+ subdomain_file_name + ".timestamp"):
+        with open("subdomain_dir/"+ subdomain_file_name + ".timestamp", "r") as ts_file:
+            timestamp = int(ts_file.read())
+        if (int(time.time()) - timestamp) <= 86400:
+            if os.path.exists("subdomain_dir/"+ subdomain_file_name):
+                cached_and_valid = True
+
+    if not cached_and_valid:
+        chaos_response = httpx.get("https://chaos-data.projectdiscovery.io/" + subdomain_file_name)
+
+        #if not 200 ok go home.
+        if chaos_response.status_code != 200:
+            return []
+
+        #make directory
+        if not os.path.exists("subdomain_dir"):
+            os.mkdir("subdomain_dir")
+        
+        #write contents to a zip file
+        with open("subdomain_dir/" + subdomain_file_name, "wb") as f:
+            f.write(chaos_response.content)
+
+        #write new timestamp
+        with open("subdomain_dir/" + subdomain_file_name + ".timestamp", "w") as ts:
+            ts.write(str(int(time.time())))
+    
+    #if it's not a zip file go home
+    if not zipfile.is_zipfile("subdomain_dir/" + subdomain_file_name):
+        os.remove("subdomain_dir/" + subdomain_file_name)
+        return []
+
+    #read and return contents
+    subdomain_list = []
+    
+    with zipfile.ZipFile("subdomain_dir/" + subdomain_file_name, "r") as zf:
+        compressed_files = zf.namelist()
+        for compressed_file_name in compressed_files:
+            with zf.open(compressed_file_name, "r") as f:
+                subdomain_list += f.read().splitlines()
+
+    return subdomain_list
+
+print(get_chaos_subdomains("84codes"))
